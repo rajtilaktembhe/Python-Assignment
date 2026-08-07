b5dce7ae120e1442f5d1fb3eb12ce87706c541d7
rajtilaktembhe <rajtilakt.ml25@sbjit.edu.in>
2026-08-07 15:49:48 +0530

    Que6

---
+++ Python Assignment1/Que6.py
@@ -0,0 +1,13 @@
+fruits = {"apple", "banana", "cherry"}
+
+# Add element
+fruits.add("orange")
+print("After adding:", fruits)
+
+# Remove element
+fruits.remove("banana")
+print("After remove:", fruits)
+
+# Discard element
+fruits.discard("apple")
+print("After discard:", fruits)
