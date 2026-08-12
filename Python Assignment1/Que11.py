38abec79ccdc12c0824e906900d5a5bfd9a3cda2
rajtilaktembhe <rajtilakt.ml25@sbjit.edu.in>
2026-08-12 20:22:03 +0530

    Que11

---
+++ Python Assignment1/Que11.py
@@ -0,0 +1,9 @@
+student = {
+    "name": "Alice",
+    "age": 20,
+    "grade": "B"
+}
+student["city"] = "Pune"
+student["grade"] = "A"
+print("Student name:", student["name"])
+print("Updated student dictionary:", student)
